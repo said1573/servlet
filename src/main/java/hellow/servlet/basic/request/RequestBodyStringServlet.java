35c8d77676cc775ce1e275850431e6c9d2331f64
said1573 <therapy23@naver.com>
2022-05-09 18:00:10 +0900

    서블릿 완료 2022.05.09 (월)

---
+++ src/main/java/hellow/servlet/basic/request/RequestBodyStringServlet.java
@@ -0,0 +1,30 @@
+package hellow.servlet.basic.request;
+
+import org.springframework.util.StreamUtils;
+
+import javax.servlet.ServletException;
+import javax.servlet.ServletInputStream;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.nio.charset.StandardCharsets;
+
+@WebServlet(name = "requestBodyStringServlet", urlPatterns = "/request-body-string")
+public class RequestBodyStringServlet extends HttpServlet {
+
+    @Override
+    protected void service(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+
+        // byte code로 변환
+        ServletInputStream inputStream = request.getInputStream();
+
+        // byte code -> String으로 변환 (변환 시 반드시 인코딩 정보를 넣어줘야함)
+        String messageBody = StreamUtils.copyToString(inputStream, StandardCharsets.UTF_8);
+
+        System.out.println("messageBody = " + messageBody);
+
+        response.getWriter().write("ok");
+    }
+}
---
+++ src/main/java/hellow/servlet/basic/response/ResponseHeaderServlet.java
@@ -0,0 +1,32 @@
+package hellow.servlet.basic.response;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.io.PrintWriter;
+
+@WebServlet(name = "responseHeaderServlet", urlPatterns = "/response-header")
+public class ResponseHeaderServlet extends HttpServlet {
+
+    @Override
+    protected void service(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+
+        //[status-line]
+        response.setStatus(HttpServletResponse.SC_OK); //200
+
+        //[response-headers]
+        response.setHeader("Content-Type", "text/plain;charset=utf-8");
+        response.setHeader("Cache-Control", "no-cache, no-store, must-revalidate");
+        response.setHeader("Pragma", "no-cache");
+        response.setHeader("my-header", "hello");
+
+        //[Header 편의 메서드] content(response); cookie(response); redirect(response);
+        //[message body]
+        PrintWriter writer = response.getWriter();
+        writer.println("ok");
+
+    }
+}
